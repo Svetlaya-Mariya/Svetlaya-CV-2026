135fbf7f329a474da4f0220a7c0f147f2466fb4b
Mariya Svetlaya <merry76light@gmail.com>
2026-03-07 22:15:14 +0300

    add language change

--- script.js
+++ script.js
@@ -10,3 +10,37 @@ exampleWrapper.addEventListener('click', (event) => {
 function showDiscription(){
     console.log('hello');
 }
+
+const langRus = document.querySelector('#rus');
+const langEng = document.querySelector('#eng');
+
+const rusVersion = document.querySelectorAll('.lang_rus');
+const engVersion = document.querySelectorAll('.lang_eng');
+
+console.log(rusVersion)
+
+langRus.addEventListener('click', changeEngToRus);
+langEng.addEventListener('click', changeRusToEng);
+
+
+function changeEngToRus(){ 
+    rusVersion.forEach((item) => {
+        item. classList.remove('hidden');
+        })
+
+    engVersion.forEach((item) => {
+        item. classList.add('hidden');
+    }
+    )
+}
+
+function changeRusToEng(){ 
+    engVersion.forEach((item) => {
+        item. classList.remove('hidden');
+        })
+
+    rusVersion.forEach((item) => {
+        item. classList.add('hidden');
+    }
+    )
+}
